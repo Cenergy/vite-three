f0e3676285301d213a1f0f74c01ca09852add7fa
cenergy <673598118@qq.com>
2022-08-12 12:17:28 +0800

    初始化

---
+++ main.js
@@ -0,0 +1,23 @@
+import "./src/assert/css/style.css";
+import javascriptLogo from "./src/assert/img/javascript.svg";
+import { setupCounter } from "./src/counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <a  target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Hello World
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
