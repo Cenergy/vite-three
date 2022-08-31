a5934430f7b4f2669aae05edeb39bcf6c3603547
cenergy <673598118@qq.com>
2022-08-31 14:11:42 +0800

    earth-moon

--- src/earthAndMoon/index.js
+++ src/earthAndMoon/index.js
@@ -24,36 +24,75 @@ dirLight.position.set(0, 0, 10);
 dirLight.castShadow = true;
 scene.add(dirLight);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+});
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 renderer.render(scene, camera);
 document.body.appendChild(renderer.domElement);
 
 const moonGeometry = new THREE.SphereGeometry(MOON_RADIUS, 16, 16);
 const moonMaterial = new THREE.MeshPhongMaterial({
-  map: textureLoader.load("/door.png"),
+  map: textureLoader.load("/textures/planets/moon_1024.jpg"),
 });
 const moon = new THREE.Mesh(moonGeometry, moonMaterial);
 scene.add(moon);
 
 const earthGeometry = new THREE.SphereGeometry(EARTH_RADIUS, 16, 16);
 const earthMaterial = new THREE.MeshPhongMaterial({
-  map: textureLoader.load("/textures/sky.jpg"),
+  shininess: 5,
+  map: textureLoader.load("/textures/planets/earth_atmos_2048.jpg"),
+  specularMap: textureLoader.load("/textures/planets/earth_specular_2048.jpg"),
+  normalMap: textureLoader.load("/textures/planets/earth_normal_2048.jpg"),
 });
-const erath = new THREE.Mesh(earthGeometry, earthMaterial);
-scene.add(erath);
+const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+earth.receiveShadow = true;
+moon.castShadow = true;
+moon.receiveShadow = true;
+earth.castShadow = true;
+scene.add(earth);
 
-const orbitControls = new OrbitControls(camera, renderer.domElement);
+renderer.domElement.style.backgroundImage = "url(/textures/starts.jpg)";
+renderer.domElement.style.backgroundSize = "cover";
 
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+labelRenderer.domElement.style.position = "absolute";
+labelRenderer.domElement.style.top = "0px";
+labelRenderer.domElement.style.left = "205px";
+labelRenderer.domElement.style.color = "#fff";
+document.body.appendChild(labelRenderer.domElement);
+
+const orbitControls = new OrbitControls(camera, labelRenderer.domElement);
 function render() {
   const time = clock.getElapsedTime();
   renderer.render(scene, camera);
+  labelRenderer.render(scene, camera);
   moon.position.set(Math.sin(time) * 5, 0, Math.cos(time) * 5);
-  erath.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 1000);
+  earth.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 1000);
   requestAnimationFrame(render);
 }
 render();
+
+// 创建标签
+
+const earthDiv = document.createElement("div");
+earthDiv.className = "label";
+earthDiv.innerHTML = "<h1>Earth</h1>";
+earthDiv.style.marginTop = "-1em";
+const earthLabel = new CSS2DObject(earthDiv);
+earthLabel.position.set(0, EARTH_RADIUS, 0);
+earth.add(earthLabel);
+
+const moonDiv = document.createElement("div");
+moonDiv.className = "label";
+moonDiv.textContent = "Moon";
+moonDiv.style.marginTop = "-1em";
+const moonLabel = new CSS2DObject(moonDiv);
+moonLabel.position.set(0, MOON_RADIUS, 0);
+moon.add(moonLabel);
 
 export default {};
