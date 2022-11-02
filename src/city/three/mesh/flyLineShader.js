08e5c23a94bc0384ca903d652467a56e18cb38f9
cenergy <673598118@qq.com>
2022-11-02 14:25:56 +0800

    add flyline animate

--- src/city/three/mesh/flyLineShader.js
+++ src/city/three/mesh/flyLineShader.js
@@ -5,20 +5,49 @@ import vertexShader from "../shader/flyLine/vertex.glsl";
 import fragmentShader from "../shader/flyLine/fragment.glsl";
 
 export default class FlyLine {
-  constructor() {
+  constructor(position = { x: 0, z: 0 }, color = 0x00ffff) {
     const linePoints = [
       new THREE.Vector3(0, 0, 0),
       new THREE.Vector3(-8, 4, 0),
       new THREE.Vector3(-12, 0, 0),
     ];
     this.lineCurve = new THREE.CatmullRomCurve3(linePoints);
-    const points = this.lineCurve.getPoints(10);
+    const points = this.lineCurve.getPoints(1000);
     this.geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const pointsLength = new Float32Array(points.length);
+    for (let index = 0; index < pointsLength.length; index++) {
+      pointsLength[index] = index;
+    }
+    this.geometry.setAttribute(
+      "aSize",
+      new THREE.BufferAttribute(pointsLength, 1)
+    );
+
     this.material = new THREE.ShaderMaterial({
+      uniforms: {
+        uTime: {
+          value: 0,
+        },
+        uColor: {
+          value: new THREE.Color(color),
+        },
+        uLength: {
+          value: points.length,
+        },
+      },
       vertexShader,
       fragmentShader,
       transparent: true,
+      depthWrite: false,
     });
     this.mesh = new THREE.Points(this.geometry, this.material);
+
+    // 改变uTime来控制动画
+    gsap.to(this.material.uniforms.uTime, {
+      value: 1000,
+      duration: 2,
+      repeat: -1,
+      ease: "none",
+    });
   }
 }
--- src/city/three/shader/flyLine/fragment.glsl
+++ src/city/three/shader/flyLine/fragment.glsl
@@ -1,5 +1,12 @@
+varying float vSize;
+uniform vec3 uColor;
+
 void main() {
     float strength = distance(gl_PointCoord, vec2(0.5, 0.5));
     strength = 1.0 - (strength * 2.0);
-    gl_FragColor = vec4(1.0, 0.0, 0.0, strength);
+    if(vSize > 0.0) {
+        gl_FragColor = vec4(uColor, strength);
+    } else {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 0.0);
+    }
 }
